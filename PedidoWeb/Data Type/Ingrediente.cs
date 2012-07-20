2b1989f28e2995181d8a34395c49369d879c6504
lbalena <l.balena@gmail.com>
2012-07-19 22:10:17 -0300

    Adicionadas classes Ingrediente e Lanche

---
+++ PedidoWeb/Data Type/Ingrediente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PedidoWeb.Data_Type
+{
+    class Ingrediente
+    {
+        private int id;
+        public int Id {
+            get { return id; }
+            set { id = value; }
+        }
+        private string ingrediente;
+        public string Ingrediente
+        {
+            get
+            { return ingrediente; }
+            set
+            { ingrediente = value; }
+        }
+        public Ingrediente()
+        {
+            this.Id = 0;
+            this.Ingrediente = null;
+        }
+    }
+}
---
+++ PedidoWeb/Data Type/Lanche.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PedidoWeb.Data_Type
+{
+    class Lanche
+    {
+        private int id;
+        private string nome;
+        private List<Ingrediente> ingredientes;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public List<Ingrediente> Ingredientes
+        {
+            get { return ingredientes; }
+            set { ingredientes = value; }
+        }
+
+        public Lanche(string nome)
+        {
+            if (id == 0)
+            {
+                this.Id = 1;
+            }
+            else
+            {
+                this.Id = id++;
+            }
+            this.Nome = nome;
+            this.Ingredientes = new List<Ingrediente>();
+        }
+
+        public void AddIngrediente(string ingrediente)
+        {
+            Ingrediente i = new Ingrediente();
+            i.Ingrediente = ingrediente;
+            this.Ingredientes.Add(i);
+        }
+    }
+}
