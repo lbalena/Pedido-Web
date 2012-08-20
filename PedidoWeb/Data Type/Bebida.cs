f97d7d0ace366730c3491188b124846d1db93a62
lbalena <l.balena@gmail.com>
2012-08-20 10:10:28 -0300

    Adicionados Bebida, Produto e Pedido. Modificadas Lanche

---
+++ PedidoWeb/Data Type/Bebida.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PedidoWeb.Data_Type
+{
+    class Bebida
+    {
+
+        private int id;
+        private string nome;
+        private float valor;
+
+        private int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public float Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public Bebida(string nome, float valor)
+        {
+            this.Nome = nome;
+            this.Valor = Valor;
+        }
+    }
+}
---
+++ PedidoWeb/Data Type/Pedido.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PedidoWeb.Data_Type
+{
+    class Pedido
+    {
+        private List<Bebida> bebidas;
+        private List<Lanche> lanches;
+        private List<Produto> consumo;
+
+        public List<Bebida> Bebidas
+        {
+            get { return bebidas; }
+            set { bebidas = value; }
+        }
+
+        public List<Lanche> Lanches
+        {
+            get { return lanches; }
+            set { lanches = value; }
+        }
+
+        public List<Produto> Consumo
+        {
+            get { return consumo; }
+            set { consumo = value; }
+        }
+
+        public Pedido()
+        {
+            Bebidas = new List<Bebida>();
+            Lanches = new List<Lanche>();
+            Consumo = new List<Produto>();
+        }
+    }
+}
