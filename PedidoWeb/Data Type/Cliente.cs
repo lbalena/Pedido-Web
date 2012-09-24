6c06868dddacb5e605d0e01a6cfe6d50b00fda06
lbalena <l.balena@gmail.com>
2012-09-24 11:39:26 -0300

    Adicionadas as classes cliente, Endereco e Telefone - Modleafem do cliente.

---
+++ PedidoWeb/Data Type/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PedidoWeb.Data_Type
+{
+    class Cliente
+    {
+        private string nome;
+        private string tel;
+        private Endereco end;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { this.nome = value; }
+        }
+
+        public string Tel
+        {
+            get { return tel; }
+            set { this.tel = value; }
+        }
+
+        public Endereco End
+        {
+            get { return end; }
+            set { this.end = value; }
+        }
+
+        public Cliente(string nome, string tel, Endereco end)
+        {
+            Nome = nome;
+            Tel = tel;
+            End = end;
+        }
+    }
+}
---
+++ PedidoWeb/Data Type/Endereco.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PedidoWeb.Data_Type
+{
+    class Endereco
+    {
+        private string logradouro;
+        private string end;
+        private string num;
+        private string referencia;
+        private string cep;
+
+        public string Logradouro
+        {
+            get { return logradouro; }
+            set { this.logradouro = value; }
+        }
+
+        public string End
+        {
+            get { return end; }
+            set { this.end = value; }
+        }
+
+        public string Num
+        {
+            get { return num; }
+            set { this.num = value; }
+        }
+
+        public string Referencia
+        {
+            get { return referencia; }
+            set { this.referencia = value; }
+        }
+
+        public string Cep
+        {
+            get { return cep; }
+            set { this.cep = value; }
+        }
+
+        public Endereco() { 
+        }
+    }
+}
